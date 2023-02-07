b4690d0d8d3a620b6967d3f8e2effd80ea410ebe
litianyi <574399358@qq.com>
2023-02-07 23:04:20 +0800

    登录注册tab

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,4 @@
 <template>
-  <h1>我是跟组件的内容</h1>
   <!--路由出口-->
   <router-view/>
 </template>
--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,21 +1,54 @@
 <template>
     <div class="login">
-        登录
-        <el-button type="primary">Primary</el-button>
+        <div class="login-con">
+            <ul class="menu-tab">
+                <li v-for="v in menuData" :class="{current: v.current}" :key="v.type" @click="clickMenu(v)">
+                    {{ v.txt }}
+                </li>
+            </ul>
+        </div>
     </div>
 </template>
 
-<script>
-export default {
-    
+<script lang="ts" setup>
+import { reactive } from "vue"
+
+const menuData = reactive([
+    { txt: "登录", current: true, type: "login" },
+    { txt: "注册", current: false, type: "login" }
+])
+
+let clickMenu = (item:any) => {
+    menuData.forEach((ele) => {
+        ele.current = false;
+    })
+    item.current = true;
 }
 </script>
 
 <style lang="scss">
-    .login {
-        height: 100%;
+.login {
+    height: 100%;
+}
+
+html,
+body,
+#app {
+    height: 100%;
+}
+.menu-tab {
+    text-align: center;
+    li {
+        display: inline-block;
+        width: 88px;
+        line-height: 36px;
+        font-size: 14px;
+        color: rgb(255, 255, 255);
+        border-radius: 2px;
     }
-    html,body,#app{
-        height: 100%;
+    .current {
+        background-color: rgb(0, 110, 255);
     }
+}
+
 </style>
