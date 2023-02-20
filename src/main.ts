ba2ab4b7d53ab4d096b61552a7b1ae92837c7c6a
litianyi <litianyi@wxchina.com>
2023-02-20 18:20:05 +0800

    优化样式

--- src/main.ts
+++ src/main.ts
@@ -4,5 +4,9 @@ import router from './router'
 import store from './store'
 
 import "lib-flexible/flexible.js"
+import ElementPlus from 'element-plus'
+import zhCn from 'element-plus/dist/locale/zh-cn.mjs'
 
-createApp(App).use(store).use(router).mount('#app')
+createApp(App).use(store).use(router).use(ElementPlus, {
+    locale: zhCn
+}).mount('#app')
--- src/views/home/movie/MovieList.vue
+++ src/views/home/movie/MovieList.vue
@@ -1,10 +1,10 @@
-<template>
-    <!-- 列表 -->
+<template><!-- 列表 -->
     <div>
-        <el-table :data="$store.state.MovieModule.movieList.records" border style="width: 100%" size="large">
+        <el-table :data="$store.state.MovieModule.movieList.records" style="width: 100%" size="large" stripe
+            :header-cell-style="headerRowStyle" :cell-style="cellStyle">
             <el-table-column prop="movieId" label="影片id" />
             <el-table-column show-overflow-tooltip="true" prop="mvName" label="影片名称" />
-            <el-table-column show-overflow-tooltip="true" prop="createTime" label="上映时间" />
+            <el-table-column show-overflow-tooltip="true" prop="createTime" label="创建时间" />
             <el-table-column show-overflow-tooltip="true" prop="updateTime" label="更新时间" />
             <el-table-column prop="categoryName" label="影片类型" />
             <el-table-column prop="mvArea.description" label="出厂地区" />
@@ -13,21 +13,29 @@
             <el-table-column show-overflow-tooltip="true" prop="picture" label="图片地址" />
             <el-table-column prop="score" label="评分" />
             <el-table-column prop="state.description" label="状态" />
+            <el-table-column label="操作" fixed="right" width="190">
+                <template #default="scope">
+                    <el-button type="primary" @click="handleEdit(scope.$index, scope.row)">修改</el-button>
+                    <el-button type="danger" @click="handleDelete(scope.$index, scope.row)">删除</el-button>
+                </template>
+            </el-table-column>
         </el-table>
     </div>
+    <br/>
     <!-- 分页 -->
-    <el-pagination v-model:current-page="currentPage" v-model:page-size="pageSize"
+    <el-pagination background v-model:current-page="currentPage" v-model:page-size="pageSize"
         layout="total, sizes, prev, pager, next, jumper" :total="$store.state.MovieModule.movieList.total"
         @size-change="handleSizeChange" @current-change="handleCurrentChange" />
+    <!-- 修改弹出框 -->
+    <UpdateDialog />
 </template>
 
 <script setup lang="ts">
 import { ref, onMounted } from 'vue';
-import link from '@/api/link';
-import apiUrl from '@/api/url';
 import { useStore } from 'vuex';
 let store = useStore();
 
+// 表格数据加载
 interface MovieListDTO {
     mvName: string,
     mvType: number,
@@ -38,7 +46,7 @@ interface MovieListDTO {
 }
 
 const currentPage = ref(1)
-const pageSize = ref(12)
+const pageSize = ref(10)
 
 let refreshList = () => {
     let data = {
@@ -60,6 +68,45 @@ const handleCurrentChange = (val: number) => {
     currentPage.value = val;
     refreshList();
 }
+// 表格数据加载
+
+// 表格操作
+interface User {
+  date: string
+  name: string
+  address: string
+}
+const handleEdit = (index: number, row: User) => {
+  console.log(index, row)
+  store.commit("SET_DIALOG",row)
+}
+
+const handleDelete = (index: number, row: User) => {
+  console.log(index, row),
+  store.dispatch("DEL_DIALOG",row)
+}
+// 表格操作
+
+// 表格样式
+let headerRowStyle = (row) => {
+    return {
+        backgroundColor: "rgb(64,158,225)",
+        color: "#fff",
+        height: "65px",
+        fontSize: "16px",
+        fontFamily: "Microsoft YaHei"
+    };
+}
+
+let cellStyle = ({ row, column, rowIndex, columnIndex }: never) => {
+    let data = {
+        height: "55px",
+        color: "#222",
+        fontFamily: "Microsoft YaHei"
+    }
+    return data;
+}
+// 表格样式
 
 </script>
 
