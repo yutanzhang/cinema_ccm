8e6a9afea048c6fe833e964dc2f955a119e4d8a4
litianyi <litianyi@wxchina.com>
2023-03-07 16:49:05 +0800

    格式化

--- src/hook/index.js
+++
@@ -1,7 +0,0 @@
-import md5 from "js-md5"
-import { ref } from "vue"
-
-export default function(data) {
-    let md5Data = ref(md5(data))
-    return md5Data;
-}
--- src/utils/verification.js
+++
@@ -1,9 +0,0 @@
-export function checkUser(data) {
-    let reg = /^[a-zA-Z][a-zA-Z0-9_]{4,15}$/
-    return !reg.test(data)? true : false;
-}
-
-export function checkPassword(data) {
-    let reg = /^[a-zA-Z]\w{5,17}$/
-    return !reg.test(data) ? true : false;
-}
