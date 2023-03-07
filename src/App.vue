c171a9999e986f902ba386bbbd8028d9c81ddcf9
litianyi <574399358@qq.com>
2023-03-07 22:51:14 +0800

    测试 pinia

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,11 @@
 <template>
-    <RouterView />
+    <n-config-provider :locale="zhCN" :date-locale="dateZhCN">
+        <RouterView />
+    </n-config-provider>
 </template>
 
 <script setup lang="ts">
-
+import { zhCN, dateZhCN } from 'naive-ui'
 
 </script>
 
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,11 +2,21 @@
   <div>
     <n-button>naive-ui</n-button>
     使用特殊类型的字体
+    <n-button @click="countAdd()">count: {{ count }}</n-button>
+    <n-button>doubleCount: {{ doubleCount }}</n-button>
   </div>
 </template>
 
 <script lang="ts" setup>
 import { NButton } from 'naive-ui'
+import { useCounterStore } from '../stores/counter'
+import { storeToRefs } from 'pinia'
+
+const store = useCounterStore();
+let {count, doubleCount} = storeToRefs(store);
+let countAdd = () => {
+  store.count++;
+}
 
 </script>
 
